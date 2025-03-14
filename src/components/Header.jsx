7e0289cd472302d3c5b746d1a6d41c196a8fbcbc
obinaligoodness <obinaligoodness@gmail.com>
2025-03-14 16:40:56 +0100

    worked on displayed error message for search that were not found

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,7 @@ import LibraryBooksOutlinedIcon from "@mui/icons-material/LibraryBooksOutlined";
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 import { Switch } from "@mui/material";
 import { ThemeContext } from "@/ThemeProvider";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 const Header = () => {
   const { font, setFont, fontOptions } = useContext(FontContext);
   const { theme, toggleTheme } = useContext(ThemeContext);
@@ -15,42 +16,48 @@ const Header = () => {
   return (
     <div className="flex justify-between py-6">
       <LibraryBooksOutlinedIcon sx={{ fontSize: "50px" }} />
-<div className="flex gap-2 justify-center items-center">
-      <Select
-        value={font}
-        onChange={(e) => setFont(e.target.value)}
-        sx={{
-          border: "none",
-          outline: "none",
-          backgroundColor: "transparent",
-          color: theme === 'dark'  ? "white" : "black",
-          "& fieldset": { border: "none" },
-        }}
-      >
-        {fontOptions.map((option, index) => (
-          <MenuItem key={index} value={option.value}>
-            {option.label}
-          </MenuItem>
-        ))}
-      </Select>
+      <div className="flex gap-2 justify-center items-center">
 
-      <div className="flex items-center justify-center gap-2">
-
-        <Switch
-          id="shuffleQuestions"
-          name="shuffleQuestions"
-          onChange={toggleTheme}
+        <Select
+          value={font}
+          onChange={(e) => setFont(e.target.value)}
+          IconComponent={KeyboardArrowDownIcon}
           sx={{
-            "& .MuiSwitch-thumb": { backgroundColor: "#757575" },
-            "& .Mui-checked .MuiSwitch-thumb": { backgroundColor: "white" },
-            "& .Mui-checked": { color: "#757575" },
-            "& .Mui-checked + .MuiSwitch-track": { backgroundColor: "#757575" },
+            border: "none",
+            outline: "none",
+            backgroundColor: "transparent",
+            color: theme === "dark" ? "white" : "black",
+            "& fieldset": { border: "none" },
+            "& .MuiSelect-icon": {
+              color: theme === "dark" ? "white" : "black",
+            },
           }}
-        />
-        <button className="cursor-pointer">
-          <DarkModeOutlinedIcon className="dark:text-white text-black" />
-        </button>
-      </div>
+        >
+          {fontOptions.map((option, index) => (
+            <MenuItem key={index} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </Select>
+
+
+        <div className="flex items-center justify-center gap-2">
+
+          <Switch
+            id="shuffleQuestions"
+            name="shuffleQuestions"
+            onChange={toggleTheme}
+            sx={{
+              "& .MuiSwitch-thumb": { backgroundColor: "#757575" },
+              "& .Mui-checked .MuiSwitch-thumb": { backgroundColor: "white" },
+              "& .Mui-checked": { color: "#757575" },
+              "& .Mui-checked + .MuiSwitch-track": { backgroundColor: "#757575" },
+            }}
+          />
+          <button className="cursor-pointer">
+            <DarkModeOutlinedIcon className="dark:text-white text-black" />
+          </button>
+        </div>
       </div>
     </div>
   );
